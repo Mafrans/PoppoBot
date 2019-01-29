da9623f363fe8b18f36221c0b6d20b87f972a774
Mafrans <malte.kluft@gmail.com>
2019-01-29 14:00:49 +0100

    Disregard last message

--- src/main/java/me/mafrans/poppo/Main.java
+++ src/main/java/me/mafrans/poppo/Main.java
@@ -110,6 +110,7 @@ public class Main {
         CommandHandler.addCommand(new Command_skip());
         CommandHandler.addCommand(new Command_smite());
         CommandHandler.addCommand(new Command_stars());
+        CommandHandler.addCommand(new Command_time());
         CommandHandler.addCommand(new Command_timeout());
         CommandHandler.addCommand(new Command_unmute());
         CommandHandler.addCommand(new Command_untimeout());
